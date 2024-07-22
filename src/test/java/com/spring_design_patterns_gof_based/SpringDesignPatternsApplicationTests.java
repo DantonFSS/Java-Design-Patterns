d5b35e96f0ea684a3c3bab9238f096c617bf420a
DantonFSS <dantonfilipe.s@gmail.com>
2024-07-21 19:30:57 -0700

    Tests

---
+++ src/test/java/com/spring_design_patterns_gof_based/SpringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_design_patterns_gof_based;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
