ffd37c757f800ad88421ed07557952a286dfddb6
DantonFSS <dantonfilipe.s@gmail.com>
2024-09-20 17:58:31 -0300

    AddressModel class

---
+++ src/main/java/com/spring_design_patterns_gof_based/model/AddressModel.java
@@ -0,0 +1,76 @@
+package com.spring_design_patterns_gof_based.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class AddressModel {
+
+    @Id
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String complemento;
+    private String localidade;
+    private String uf;
+    private String ddd;
+
+    public AddressModel() {
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public void setLocalidade(String localidade) {
+        this.localidade = localidade;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+}
